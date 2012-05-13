e04de9e4a071c339d40c6f6b0d7d86b556db4358
Filipe Cristóvão <filipe.md.cristovao@gmail.com>
2012-05-13 18:05:13 +0100

    Taking out the remnants of (commented) code from Akka 1.3

--- src/main/scala/kadact/node/Contact.scala
+++ src/main/scala/kadact/node/Contact.scala
@@ -1,37 +1,8 @@
 package kadact.node
 
 import akka.actor.ActorRef
-import java.io.IOException
-import java.io.ObjectOutputStream
-import java.io.ObjectInputStream
-
-//import akka.serialization.RemoteActorSerialization._
 
 case class Contact(var nodeID: NodeID, var node: ActorRef) {
-	
-	/*
-	private def nodeID_=(newNodeID: NodeID) : Unit = {
-		nodeID = newNodeID 
-	}
-	
-	
-	@throws(classOf[IOException])
-	private def writeObject(out: ObjectOutputStream){
-		out.writeObject(nodeID)
-		val tmp = toRemoteActorRefProtocol(node).toByteArray()
-		out.writeInt(tmp.size)
-		out.write(tmp)
-	}
-	
-	@throws(classOf[IOException]) @throws(classOf[ClassNotFoundException])
-	private def readObject(in: ObjectInputStream){
-		nodeID = in.readObject().asInstanceOf[NodeID];
-		val tmp = in.readInt()
-		val tmpArray = new Array[Byte](tmp)
-		in.read(tmpArray,0,tmp)
-		node = fromBinaryToRemoteActorRef(tmpArray)
-	}
-	*/
 	override def toString(): String = {
 		"Contact("+nodeID+")"
 	}
