7aef9086856a7d101249ef31a535ba162ec29275
Filipe Cristóvão <filipe.md.cristovao@gmail.com>
2013-06-23 21:39:08 +0200

    Some more test stubs
    Still not useful at all...

--- src/test/scala/kadact/KadActSpec.scala
+++ src/test/scala/kadact/KadActSpec.scala
@@ -1,11 +1,11 @@
 package kadact
 
-import org.scalatest.FunSpec
+import org.scalatest.WordSpec
 import kadact.config.KadActConfig
 import scala.collection.JavaConversions.mapAsJavaMap
 import com.typesafe.config.ConfigFactory
 
-class KadActSpec extends FunSpec {
+class KadActSpec extends WordSpec {
 
 	implicit val config = new KadActConfig(ConfigFactory.parseMap(
 																			Map("kadact.alpha" -> 3, 
@@ -15,15 +15,47 @@ class KadActSpec extends FunSpec {
 																					"kadact.timeouts.nodeLookup" -> 5
 																					)))
 	
-	describe("Kadact") {
-		ignore("should be initializable") {
+	def createKadActNetwork(howManyNodes: Int) = {
+		require(howManyNodes > 0)
+		val result = new Array[KadAct[Int]](howManyNodes)
+		val startingPort = 55372
+		
+		result(0) = new KadAct[Int]("127.0.0.1", startingPort)
+		result(0).start()
+		Thread.sleep(5000)
+		
+		for(currentIndex <- 1 to (howManyNodes - 1)) {
+			result(currentIndex) = new KadAct[Int]("127.0.0.1", startingPort + currentIndex)
+			result(currentIndex).join("127.0.0.1", startingPort + currentIndex - 1)
+			Thread.sleep(5000)
+		}
+		result
+	}
+	
+	def stopKadActNetwork(network: Array[KadAct[Int]]) = {
+		for(index <- (network.length - 1) to 1 by -1){
+			network(index).stop()
+			Thread.sleep(1000)
+		}
+	}
+	
+	
+	"A KadAct node" should {
+		"be initializable" ignore {
 			new KadAct[Int]("localhost", 55372)
 		}
 		
-		it("should be possible to add content to the network") {
+		"be able to add content to the network" ignore {
 			val kadact = new KadAct[Int]("localhost", 55373)
 			kadact.start()
 			kadact.add(0,1)
 		}
+		
+		"be able to join a network of KadAct nodes" in {
+			val kadActNetwork = createKadActNetwork(2)
+			stopKadActNetwork(kadActNetwork)
+		}
 	}
+	
+	
 }
