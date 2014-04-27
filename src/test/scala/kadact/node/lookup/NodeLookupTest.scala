a289cd8c76af3728f2a643696408f777bd801ece
Filipe Cristóvão <filipe.md.cristovao@gmail.com>
2014-04-27 19:21:30 +0200

    Refactors a common test function to the package object

--- src/test/scala/kadact/node/lookup/NodeLookupTest.scala
+++ src/test/scala/kadact/node/lookup/NodeLookupTest.scala
@@ -1,15 +1,16 @@
 package kadact.node.lookup
 
 import akka.testkit.{TestProbe, ImplicitSender, TestKit}
-import akka.actor.{Props, Actor, ActorSystem}
+import akka.actor.{Props, ActorSystem}
 import com.typesafe.config.ConfigFactory
 import org.scalatest.{BeforeAndAfterAll, MustMatchers, WordSpecLike}
 import kadact.config.TestKadActConfig
 import kadact.config.modules.{LookupManagerModule, RoutingTableModule}
 import akka.util.Timeout
 import scala.concurrent.duration._
-import kadact.node.{NodeID, Contact, KadActNode}
+import kadact.node.Contact
 import kadact.node.routing.RoutingTable.PickNNodesCloseTo
+import kadact.nodeForTest._
 
 class NodeLookupTest extends TestKit(ActorSystem("test", ConfigFactory.load("application-test")))
                              with WordSpecLike
@@ -45,7 +46,7 @@ class NodeLookupTest extends TestKit(ActorSystem("test", ConfigFactory.load("app
     "there's two nodes in the network" must {
       "return the two (<= alpha) existing nodes in the network" in {
         /* Two nodes, Ids 0 and 8 */
-        val (probes, contacts) = mockContacts(BigInt(0), BigInt(8))
+        val (probes, contacts) = mockContacts(0, 8)
 
         val nodeLookup = system.actorOf(Props(new NodeLookup(contacts(0), mockRoutingTable.ref)))
         val nodeId = BigInt(0)
@@ -56,19 +57,13 @@ class NodeLookupTest extends TestKit(ActorSystem("test", ConfigFactory.load("app
         mockRoutingTable.reply(Set[Contact](contacts(1)))
 
         probes(1).expectMsg(kadact.messages.FindNode(contacts(0), generation, nodeId))
-        probes(1).reply(kadact.messages.FindNodeResponse(contacts(1), generation, Set(contacts(1))))  //returns itself as the only contact
+        probes(1).reply(
+          kadact.messages.FindNodeResponse(contacts(1), generation, Set(contacts(1)))
+        ) //returns itself as the only contact
 
         expectMsg(NodeLookup.LookupNodeResponse(generation, nodeId, Set[Contact](contacts(0), contacts(1))))
       }
     }
-  }
-
-  def mockContacts(nodeIds : NodeID*) = {
-    val probesAndContacts =
-      for (nodeId <- nodeIds;
-           testProbe = TestProbe())
-        yield (testProbe, Contact(nodeId, testProbe.ref))
-    probesAndContacts.unzip
   }
 
 }
--- src/test/scala/kadact/node/package.scala
+++ src/test/scala/kadact/node/package.scala
@@ -1,7 +1,17 @@
 package kadact
 
 import kadact.node._
+import akka.testkit.TestProbe
+import akka.actor.ActorSystem
 
 package object nodeForTest {
   implicit def intToKey(int: Int): Key = BigInt(int)
+
+  def mockContacts(nodeIds : Int*)(implicit system: ActorSystem) = {
+    val probesAndContacts =
+      for (nodeId <- nodeIds;
+           testProbe = TestProbe())
+      yield (testProbe, Contact(nodeId, testProbe.ref))
+    probesAndContacts.unzip
+  }
 }
